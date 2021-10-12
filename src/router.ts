3845557f67af2d4299a94e85cd37630c4c3c359f
daniphant <daniphnt_dev@yahoo.com>
2021-10-12 13:30:56 -0300

    fix: create user endpoint unexpected behavior

--- src/router.ts
+++ src/router.ts
@@ -3,9 +3,9 @@ import { createUserController } from './useCases/CreateUser'
 const router = Router()
 
 const userRouter = Router()
-
-userRouter.post('/create', createUserController.handle)
+userRouter.post('/create', (req, res) => {
+  return createUserController.handle(req, res)
+})
 
 router.use('/users', userRouter)
-
 export { router }
